2873e491b24a5d1c863c16a9e565bd2d4910d7b0
Marko Käär <kaarmarko@gmail.com>
2022-10-13 10:43:22 +0300

    NavBar basic

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,22 +1,40 @@
 // @flow
 import * as React from 'react';
+import ButtonGroup from 'react-bootstrap/esm/ButtonGroup';
 import Container from 'react-bootstrap/esm/Container';
+import Dropdown from 'react-bootstrap/esm/Dropdown';
 import Navbar from 'react-bootstrap/esm/Navbar';
-import {FaUser} from "react-icons/all";
+import {FaCog, FaHome, FaUser, FaUserEdit} from "react-icons/all";
+import {Link} from 'react-router-dom';
 
-type Props = {};
-export const NavBar = (props: Props) => {
-
+export const NavBar = () => {
     return (
-        <Navbar>
+        <Navbar bg="light mx-1 shadow-sm">
             <Container>
-                <Navbar.Brand href="#home">qTrack</Navbar.Brand>
-                <Navbar.Toggle/>
-                <Navbar.Collapse className="justify-content-end">
-                    <Navbar.Text>
-                        <a href="/"><FaUser /></a>
-                    </Navbar.Text>
-                </Navbar.Collapse>
+                <Link to="/" className="navbar-brand">
+                    q<span className="text-warning">Track</span>
+                </Link>
+
+                <ButtonGroup aria-label="Time picker">
+                    <Link to="/d" className="btn btn-outline-dark">Day</Link>
+                    <Link to="/w" className="btn btn-dark">Week</Link>
+                    <Link to="/m" className="btn btn-outline-dark">Month</Link>
+                </ButtonGroup>
+
+                <Dropdown align={"end"}>
+                    <Dropdown.Toggle variant="outline-dark" id="menu-dropdown">
+                        <FaUser size={24}/>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                        <Link to="/" className="dropdown-item"><FaHome/> Home</Link>
+                        <Link to="/profile" className="dropdown-item"><FaUserEdit/> Profile</Link>
+                        <Link to="/settings" className="dropdown-item"><FaCog/> Settings</Link>
+                        <Dropdown.Divider/>
+                        <Link to="/logout" className="dropdown-item">Log out</Link>
+                    </Dropdown.Menu>
+                </Dropdown>
+
             </Container>
         </Navbar>
     );
